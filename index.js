1eabfbe80941b9dbea8f46a57f6c0517425c4015
Andharikar9011 <48117912+Andharikar9011@users.noreply.github.com>
2023-01-30 21:44:48 +0530

    cors

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ import indexRouter from "./routes/index.js";
 import mongoose from "mongoose";
 import path from "path";
 import { fileURLToPath } from "url";
+import cors from "cors";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
-const port = 3000;
+const port = 8080;
 
 const uri =
   "mongodb+srv://dummy:dummy%40123@cluster0.2unng.mongodb.net/MUDB?retryWrites=true&w=majority";
@@ -25,7 +26,7 @@ mongoose
     console.log("Connected sucessfully");
   })
   .catch((err) => console.log(err));
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../public")));
